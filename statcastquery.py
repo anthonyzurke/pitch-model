7521a87635aad73a0549fbf4a6e527c4796de388
anthonyzurke <anthony.zurke1@gmail.com>
2022-02-11 13:28:11 -0500

    updates

--- statcastquery.py
+++ statcastquery.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 from pybaseball import statcast, statcast_pitcher_spin
 
 data = statcast(start_dt = '2021-04-01', end_dt = '2021-10-04')
@@ -54,3 +48,34 @@ data['pfx_z'] = 12 * data['pfx_z']
 
 data.to_csv('./data/mlb-pitches.csv')
 
+# Clean Dataset
+pd.set_option('max_columns', None)
+arsenal = pd.read_csv('./data/pitch-arsenal-stats.csv')
+arsenal = arsenal.sort_values(by = ['last_name'], ascending = True)
+#print(arsenal.shape)
+
+spin = pd.read_csv('./data/spin-direction-pitches.csv')
+spin.drop(columns = ['year', 'hawkeye_measured_clock_minutes', 'movement_inferred_clock_minutes'], inplace = True)
+spin = spin.sort_values(by = ['last_name'], ascending = True)
+spin.rename(columns={'api_pitch_type': 'pitch_type', 'api_pitch_name': 'pitch_name'}, inplace=True)
+#print(spin.shape)
+
+df = pd.merge(arsenal, spin, how = 'inner', on = ['player_id', 'pitch_type', 'pitch_type'])
+df.drop_duplicates(subset = ['player_id', 'pitch_type'], inplace = True)
+df = df[['last_name_x', 'last_name_y', ' first_name_x', 'player_id', 'pitch_hand',
+         'pitch_type', 'pitch_name_x', 'pitch_name_y', 'pitches', 'n_pitches', 'pitch_usage',
+         'release_speed', 'spin_rate', 'movement_inches', 'active_spin_formatted', 'alan_active_spin_pct', 
+         'active_spin', 'hawkeye_measured', 'movement_inferred', 'diff_measured_inferred', 'diff2', 
+         'run_value_per_100', 'run_value', 'pa', 'ba', 'slg', 'woba', 'whiff_percent', 'k_percent', 
+         'put_away', 'est_ba', 'est_slg', 'est_woba', 'hard_hit_percent', 'diff_measured_inferred_minutes', 
+         'hawkeye_measured_clock_hh', 'hawkeye_measured_clock_mm', 'movement_inferred_clock_hh', 
+         'movement_inferred_clock_mm', 'diff_clock_hh', 'diff_clock_mm', 'hawkeye_measured_clock_label', 
+         'movement_inferred_clock_label', 'diff_clock_label', 'team_name_alt']]
+#print(df.shape)
+
+col_dict = {
+    'active_spin_formatted': 'spin_eff%', 
+}
+
+df.rename(columns = col_dict, inplace = True)
+df.to_csv('./data/arsenal-spin.csv')
