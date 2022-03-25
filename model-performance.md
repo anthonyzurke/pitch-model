000d4c254840c6a63e65eaf11069882bbbb07121
anthonyzurke <anthony.zurke1@gmail.com>
2022-03-24 20:58:37 -0400

    updates

--- model-performance.md
+++ model-performance.md
@@ -5,26 +5,49 @@
 
 Model: Linear Regression
 
+Test Data
 |Pitch Type           |R2 Score  |MSE       |RMSE       |MAE       |  
 |--                   |---       |--        |---        |---       |
 |4-Seam - RHP         |0.012     |0.093     |0.304      |0.157     |
 |4-Seam - LHP         |0.010     |0.090     |0.301      |0.156     |
-|Cutter - RHP         |0.     |0.     |0.     |0.     |
-|Cutter - LHP         |0.     |0.     |0.     |0.     |
-|Sinker - RHP         |0.     |0.     |0.     |0.     |
-|Sinker - LHP         |0.     |0.     |0.     |0.     |
-|Slider - RHP         |0.     |0.     |0.     |0.     |
-|Slider - LHP         |0.     |0.     |0.     |0.     |
-|Curveball - RHP      |0.     |0.     |0.     |0.     |
-|Curveball - LHP      |0.     |0.     |0.     |0.     |
-|Changeup - RHP       |0.     |0.     |0.     |0.     |
-|Changeup - LHP       |0.     |0.     |0.     |0.     |
-|Fastball - RHP       |0.     |0.     |0.     |0.     |
-|Fastball - LHP       |0.     |0.     |0.     |0.     |
-|Breaking Ball - RHP  |0.     |0.     |0.     |0.     |
-|Breaking Ball - LHP  |0.     |0.     |0.     |0.     |
-|Off Speed - RHP      |0.     |0.     |0.     |0.     |
-|Off Speed - LHP      |0.     |0.     |0.     |0.     |
+|Cutter - RHP         |0.005     |0.092     |0.304      |0.152     |
+|Cutter - LHP         |0.004     |0.101     |0.318      |0.162     |
+|Sinker - RHP         |0.003     |0.099     |0.315      |0.174     |
+|Sinker - LHP         |0.002     |0.093     |0.305      |0.165     |
+|Slider - RHP         |0.015     |0.084     |0.290      |0.139     |
+|Slider - LHP         |0.015     |0.081     |0.285      |0.139     |
+|Curveball - RHP      |0.016     |0.073     |0.270      |0.124     |
+|Curveball - LHP      |0.016     |0.067     |0.260      |0.124     |
+|Changeup - RHP       |0.017     |0.086     |0.293      |0.150     |
+|Changeup - LHP       |0.021     |0.094     |0.306      |0.158     |
+|Fastball - RHP       |0.007     |0.096     |0.310      |0.161     |
+|Fastball - LHP       |0.004     |0.096     |0.310      |0.162     |
+|Breaking Ball - RHP  |0.015     |0.077     |0.277      |0.132     |
+|Breaking Ball - LHP  |0.014     |0.076     |0.276      |0.133     |
+|Off Speed - RHP      |0.019     |0.082     |0.287      |0.147     |
+|Off Speed - LHP      |0.022     |0.095     |0.308      |0.156     |
+
+Original Data
+|Pitch Type           |R2 Score  |MSE       |RMSE       |MAE       |  
+|--                   |---       |--        |---        |---       |
+|4-Seam - RHP         |0.012     |0.095     |0.309      |0.159     |
+|4-Seam - LHP         |0.010     |0.094     |0.307      |0.159     |
+|Cutter - RHP         |0.004     |0.090     |0.300      |0.150     |
+|Cutter - LHP         |0.003     |0.097     |0.312      |0.160     |
+|Sinker - RHP         |0.003     |0.096     |0.311      |0.172     |
+|Sinker - LHP         |0.002     |0.094     |0.307      |0.168     |
+|Slider - RHP         |0.015     |0.081     |0.285      |0.138     |
+|Slider - LHP         |0.014     |0.081     |0.284      |0.138     |
+|Curveball - RHP      |0.014     |0.068     |0.262      |0.120     |
+|Curveball - LHP      |0.016     |0.070     |0.265      |0.126     |
+|Changeup - RHP       |0.017     |0.088     |0.296      |0.149     |
+|Changeup - LHP       |0.021     |0.090     |0.301      |0.155     |
+|Fastball - RHP       |0.007     |0.095     |0.309      |0.161     |
+|Fastball - LHP       |0.005     |0.095     |0.308      |0.160     |
+|Breaking Ball - RHP  |0.015     |0.077     |0.277      |0.132     |
+|Breaking Ball - LHP  |0.014     |0.076     |0.277      |0.132     |
+|Off Speed - RHP      |0.019     |0.085     |0.292      |0.148     |
+|Off Speed - LHP      |0.021     |0.090     |0.301      |0.155     |
 
 ### model-pitch-quality-svr
 **Independent Variables:** Velocity, Spin Rate, HB, VB, Horizontal Release Position, Vertical Release, Release Extension, Horizontal Plate Coords, Vertical Plate Coords 
--- statcastquery.py
+++ statcastquery.py
@@ -12,6 +12,10 @@ data.drop(columns = ['spin_dir', 'spin_rate_deprecated', 'break_angle_deprecated
                      'fielder_8', 'fielder_9', 'bat_score', 'fld_score', 'post_bat_score', 
                      'post_fld_score'], inplace = True)
 
+data.rename(columns = {'estimated_ba_using_speedangle': 'xba',
+                       'estimated_woba_using_speedangle': 'xwoba'}, 
+            inplace = True)
+
 # Add bauer_units column
 data['bauer_units'] = data['release_spin_rate'] / data['release_speed']
 
@@ -26,10 +30,10 @@ data['pitch_count'] = data[['balls', 'strikes']].astype(str).agg('-'.join, axis 
 
 data['description'].replace(['blocked_ball', 'foul_tip', 'missed_bunt' 'swinging_strike_blocked', 'foul_bunt', 
                              'bunt_foul_tip', 'foul_pitchout'], 
-                            ['ball', 'foul', 'swinging_strike', 'swinging_strike','foul', 'foul'], inplace = True)
+                            ['ball', 'foul', 'swinging_strike', 'swinging_strike', 'foul', 'foul'],inplace = True)
 # Make all events that aren't hits, outs
-data['events'].replace(['grounded_into_double_play', 'sac_fly', 'force_out', 'hit_by_pitch', 
-                        'fielders_choice', 'fielders_choice_out'], 'field_out', inplace = True)
+data['events'].replace(['grounded_into_double_play', 'sac_fly', 'force_out', 'fielders_choice', 
+                        'fielders_choice_out'], 'field_out', inplace = True)
 # make swing_miss column
 data['swing_miss'] = [1 if x == 'swinging_strike' else 0 for x in data['description']]
 
@@ -37,6 +41,12 @@ data['swing_miss'] = [1 if x == 'swinging_strike' else 0 for x in data['descript
 # Catcher's POV: (plate_x, plate_z)
 # Pitcher's POV: (plate_-x, plate_z)
 data['plate_-x'] = -data['plate_x']
+
+# Switch from catcher's perspective to pitcher's perspective
+# Catcher's POV: (release_pos_x, release_pos_z)
+# Pitcher's POV: (release_pos_-x, release_pos_z)
+data['release_pos_-x'] = -data['release_pos_x']
+
 # Switch HB to perspective of pitcher
 # Catcher's POV: (pfx_x, pfx_z)
 # Pitcher's POV: (pfx_-x, pfx_z)
@@ -48,55 +58,3 @@ data['pfx_-x'] = 12 * data['pfx_-x']
 data['pfx_z'] = 12 * data['pfx_z']
 
 data.to_csv('../data/mlb-pitches.csv')
-
-# Clean data for modeling
-
-pitch = pd.read_csv('../data/mlb-pitches.csv', index_col = [0])
-pitch = pitch[['player_name', 'p_throws', 'pitch_type','release_speed', 'release_spin_rate', 'spin_axis', 
-               'pfx_-x', 'pfx_z', 'bauer_units', 'effective_speed', 'release_pos_x', 'release_pos_z', 
-               'release_extension', 'release_pos_y', 'plate_-x', 'plate_x', 'plate_z', 'type', 'balls','strikes', 
-               'pitch_count', 'stand', 'description', 'events', 'hit_distance_sc', 'launch_speed','launch_angle', 
-               'launch_speed_angle', 'estimated_ba_using_speedangle', 'estimated_woba_using_speedangle', 
-               'woba_value', 'woba_denom', 'babip_value', 'iso_value','at_bat_number', 'pitch_number', 
-               'inning', 'inning_topbot', 'home_score', 'away_score', 'post_home_score', 'post_away_score', 
-               'on_1b', 'on_2b', 'on_3b', 'outs_when_up', 'delta_run_exp']].copy()
-
-#Rename some columns
-col_dict = {
-    'release_speed': 'velo',
-    'release_spin_rate': 'spin_rate',
-    'launch_speed': 'exit_velo',
-    'estimated_ba_using_speedangle': 'xba',
-    'estimated_woba_using_speedangle': 'xwobacon'
-}
-pitch.rename(columns = col_dict, inplace = True)
-pitch.to_csv('../data/model-pitches.csv')
-
-# Run Expectany Table
-
-# 2010-2015 Run Expectancy
-# matrix = [[0, 0, 0, 0, 0.481], [1, 0, 0, 0, 0.859], [0, 1, 0, 0, 1.100], [1, 1, 0, 0, 1.437], 
-#           [0, 0, 1, 0, 1.350], [1, 0, 1, 0, 1.784], [0, 1, 1, 0, 1.964], [1, 1, 1, 0, 2.292], 
-#           [0, 0, 0, 1, 0.254], [1, 0, 0, 1, 0.509], [0, 1, 0, 1, 0.664], [1, 1, 0, 1, 0.884], 
-#           [0, 0, 1, 1, 0.950], [1, 0, 1, 1, 1.130], [0, 1, 1, 1, 1.376], [1, 1, 1, 1, 1.541],
-#           [0, 0, 0, 2, 0.098], [1, 0, 0, 2, 0.224], [0, 1, 0, 2, 0.319], [1, 1, 0, 2, 0.429], 
-#           [0, 0, 1, 2, 0.353], [1, 0, 1, 2, 0.478], [0, 1, 1, 2, 0.580], [1, 1, 1, 2, 0.752]]
-
-# 2019 Run Expectancy
-matrix = [[0, 0, 0, 0, 0.544], [1, 0, 0, 0, 0.935], [0, 1, 0, 0, 1.147], [1, 1, 0, 0, 1.537], 
-          [0, 0, 1, 0, 1.369], [1, 0, 1, 0, 1.759], [0, 1, 1, 0, 1.971], [1, 1, 1, 0, 2.362],
-          [0, 0, 0, 1, 0.298], [1, 0, 0, 1, 0.564], [0, 1, 0, 1, 0.713], [1, 1, 0, 1, 0.979], 
-          [0, 0, 1, 1, 0.953], [1, 0, 1, 1, 1.219], [0, 1, 1, 1, 1.368], [1, 1, 1, 1, 1.634],
-          [0, 0, 0, 2, 0.115], [1, 0, 0, 2, 0.242], [0, 1, 0, 2, 0.339], [1, 1, 0, 2, 0.467], 
-          [0, 0, 1, 2, 0.391], [1, 0, 1, 2, 0.518], [0, 1, 1, 2, 0.615], [1, 1, 1, 2, 0.743]]
-
-re = pd.DataFrame(matrix, columns = ['on_1b', 'on_2b', 'on_3b', 'outs_when_up', 're'])
-re.to_csv('../data/run_expectancy_table.csv')
-
-# wOBA by Count
-
-count_woba = [['0-0', 0.331], ['0-1', 0.281], ['0-2', 0.207], ['1-0', 0.374], ['1-1', 0.313], ['1-2', 0.233], 
-              ['2-0', 0.444], ['2-1', 0.372], ['2-2', 0.282], ['3-0', 0.588], ['3-1', 0.488], ['3-2', 0.387]]
-
-woba_value = pd.DataFrame(count_woba, columns = ['pitch_count', 'count_woba_value'])
-woba_value.to_csv('../data/count_woba_value.csv')
